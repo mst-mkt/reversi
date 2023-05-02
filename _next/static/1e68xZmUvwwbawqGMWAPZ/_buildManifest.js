290ce9ff6b1c9a34b3bf34d58e9a952c20043f21
1f10230305 <1f10230305@users.noreply.github.com>
2023-05-02 11:07:00 +0000

    deploy: a41a03849dc8f346b3d31555b8a3f2cf65b46f89

--- _next/static/1e68xZmUvwwbawqGMWAPZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/85dc6d818b680925.css","static/chunks/pages/index-861396d7a2a1aa95.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
