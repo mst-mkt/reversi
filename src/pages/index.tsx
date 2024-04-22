b1dbaf8a0115b210ca685be97b85589252402b53
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-04-23 05:51:58 +0900

    add: reversi logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,51 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+  const directions = [
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+  ];
+
+  const handleClick = (x: number, y: number) => {
+    if (board[y][x] !== 0) return;
+
+    const newBoard = structuredClone(board);
+
+    for (const [dx, dy] of directions) {
+      if (board[y + dy]?.[x + dx] === 3 - turnColor) {
+        let nx = x + dx;
+        let ny = y + dy;
+        while (board[ny]?.[nx] === 3 - turnColor) {
+          nx += dx;
+          ny += dy;
+        }
+        if (board[ny]?.[nx] === turnColor) {
+          while (nx !== x || ny !== y) {
+            newBoard[ny][nx] = turnColor;
+            nx -= dx;
+            ny -= dy;
+          }
+          newBoard[y][x] = turnColor;
+        }
+      }
+    }
+
+    setBoard(newBoard);
+    setTurnColor(3 - turnColor);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((cell, x) => (
-            <div className={styles.cell} key={`${x}-${y}`}>
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => handleClick(x, y)}>
               {cell !== 0 && (
                 <div
                   className={styles.stone}
